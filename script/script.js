69fae5ede75f0669723802f8bcac606e18a59702
drobunator <56366444+drobunator@users.noreply.github.com>
2019-12-07 19:55:14 +0200

    responsive menu add

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function () {
         accordion[i].classList.toggle('accordion-title_active');
         accordion[i].lastElementChild.classList.toggle('accordion-title__icon_active');
-        accordion[i].nextElementSibling.classList.toggle('accordion-content_active');
+       accordion[i].nextElementSibling.classList.toggle('accordion-content_active');
     });
 }
 
