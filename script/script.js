92b4132fe34ff75d2bd4fdda59ab9df26af59a07
drobunator <drobunator@gmail.com>
2020-01-08 23:56:58 +0200

    Addedd new js file

--- script/script.js
+++ script/script.js
@@ -1,19 +1,18 @@
-let accordion = document.getElementsByClassName('accordion-title');
-
+const accordion = document.getElementsByClassName("accordion-title");
+const accordionContent = document.querySelectorAll(".accordion-content");
+const accordionIcon = document.querySelectorAll(".accordion-title__icon");
 
 for (let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
-        accordion[i].classList.toggle('accordion-title_active');
-        accordion[i].lastElementChild.classList.toggle('accordion-title__icon_active');
-       accordion[i].nextElementSibling.classList.toggle('accordion-content_active');
-    });
+  accordion[i].addEventListener("click", function() {
+    accordion[i].classList.toggle("accordion-title_active");
+    accordionIcon[i].classList.toggle("accordion-title__icon_active");
+    accordionContent[i].classList.toggle("accordion-content_active");
+  });
 }
 
+const navBtn = document.querySelector(".navigation-button");
+const navigation = document.querySelector(".navigation");
 
-let navBtn = document.getElementsByClassName('navigation-button');
-
-for (let i = 0; i < navBtn.length; i++) {
-    navBtn[i].addEventListener('click', function () {
-        navBtn[i].nextElementSibling.classList.toggle('navigation_active')
-    });
-}
+navBtn.addEventListener("click", function() {
+  navigation.classList.toggle("navigation_active");
+});
